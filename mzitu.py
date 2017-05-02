238cd002acdd499e23aaed9e0ca6cdae236dc941
chenjiandongx <chenjiandongx@qq.com>
2017-05-02 09:03:37 +0800

    clean code

--- mzitu.py
+++ mzitu.py
@@ -12,11 +12,10 @@ headers = {'X-Requested-With': 'XMLHttpRequest',
                          'Chrome/56.0.2924.87 Safari/537.36'}
 
 def get_urls():
-    """
-    获取 mzitu 网站下所有套图的 url
-    :return: 去重套图 urls set
-    """
-    page_urls = ['http://www.mzitu.com/page/{cnt}'.format(cnt=str(cnt)) for cnt in range(1, 143)]
+    """ 获取 mzitu 网站下所有套图的 url """
+
+    page_urls = ['http://www.mzitu.com/page/{cnt}'.format(cnt=cnt) for cnt in range(1, 143)]
+    print("Please wait for second ...")
 
     img_urls = []
     for page_url in page_urls:
@@ -29,17 +28,14 @@ def get_urls():
         except Exception as e:
             print(e)
 
-    return set(img_urls)
+    return set(img_urls)    # 利用 set 去重 urls
 
 
 lock = threading.Lock()     # 全局资源锁
 
-
 def urls_crawler(url):
-    """
-    爬虫入口，主要爬取操作
-    :param url: 套图的地址
-    """
+    """ 爬虫入口，主要爬取操作 """
+
     try:
         response = requests.get(url, headers=headers, timeout=10).text
         folder_name = BeautifulSoup(response, 'lxml').find('div', class_="main-image").find('img')['alt'].replace("?", " ")
@@ -64,30 +60,25 @@ def urls_crawler(url):
 
 
 def save_pic(pic_src, pic_cnt):
-    """
-    保存图片到本地
-    :param pic_src: 图片地址
-    :param pic_cnt: 图片序数
-    """
+    """ 保存图片到本地 """
+
     try:
         img = requests.get(pic_src, headers=headers, timeout=10)
-        with open("pic_cnt_" + str(pic_cnt + 1) + '.jpg', 'ab') as f:
+        imgname = "pic_cnt_{}.jpg".format(pic_cnt + 1)
+        with open(imgname, 'ab') as f:
             f.write(img.content)
-            print("pic_cnt_" + str(pic_cnt + 1) + '.jpg')
+            print(imgname)
 
     except Exception as e:
         print(e)
 
 
 def make_dir(folder_name):
-    """
-    新建文件夹并切换到该目录下
-    :param folder_name: 套图名也做文件夹名
-    :return: 返回 True 如果文件夹不存在，存在则返回 False
-    """
+    """ 新建文件夹并切换到该目录下 """
+
     path = os.path.join(r"E:\mzitu", folder_name)
 
-    # 如果目录已经存在就不用再次爬取了，去重，提高效率
+    # 如果目录已经存在就不用再次爬取了，去重，提高效率。存在返回 False，否则反之
     if not os.path.exists(path):
         os.makedirs(path)
         print(path)
@@ -105,13 +96,13 @@ def delete_empty_dir(dir):
     if os.path.exists(dir):
         if os.path.isdir(dir):
             for d in os.listdir(dir):
-                path = os.path.join(dir, d)
+                path = os.path.join(dir, d)     # 组装下一级地址
                 if os.path.isdir(path):
-                    delete_empty_dir(path)
+                    delete_empty_dir(path)      # 递归删除空文件夹
 
         if not os.listdir(dir):
             os.rmdir(dir)
-            print("remove the empty dir: " + dir)
+            print("remove the empty dir: {}".format(dir))
     else:
         print("Please start your performance!")
 
@@ -123,7 +114,7 @@ if __name__ == "__main__":
     try:
         delete_empty_dir(r"E:\mzitu")
         results = pool.map(urls_crawler, urls)
-    except Exception as exception:
+    except Exception as e:
         time.sleep(30)
         delete_empty_dir(r"E:\mzitu")
         results = pool.map(urls_crawler, urls)
