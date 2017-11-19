0de230cb4ef8b59db91453504dba70bbe48185b4
chenjiandongx <chenjiandongx@qq.com>
2017-11-19 10:28:47 +0800

    代码整理

--- mzitu.py
+++ mzitu.py
@@ -7,24 +7,28 @@ from multiprocessing import Pool, cpu_count
 import requests
 from bs4 import BeautifulSoup
 
-headers = {
+HEADERS = {
     'X-Requested-With': 'XMLHttpRequest',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                  'Chrome/56.0.2924.87 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 '
+                  '(KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
     'Referer': 'http://www.mzitu.com'
 }
-dir_path = r"D:\mzitu"      # 下载图片保存路径
+
+DIR_PATH = r"D:\mzitu"      # 下载图片保存路径
 
 
 def get_urls():
-    """ 获取 mzitu 网站下所有套图的 url """
-    page_urls = ['http://www.mzitu.com/page/{cnt}'.format(cnt=cnt) for cnt in range(1, 143)]
+    """ 获取 mzitu 网站下所有套图的 url
+    """
+    page_urls = ['http://www.mzitu.com/page/{cnt}'.format(cnt=cnt)
+                 for cnt in range(1, 143)]
     print("Please wait for second ...")
     img_urls = []
     for page_url in page_urls:
         try:
-            bs = BeautifulSoup(requests.get(page_url, headers=headers, timeout=10).text,
-                               'lxml').find('ul', id="pins")
+            bs = BeautifulSoup(
+                requests.get(page_url, headers=HEADERS, timeout=10).text,
+                'lxml').find('ul', id="pins")
             result = re.findall(r"(?<=href=)\S+", str(bs))      # 匹配所有 urls
             img_url = [url.replace('"', "") for url in result]
             img_urls.extend(img_url)
@@ -32,13 +36,15 @@ def get_urls():
             print(e)
     return set(img_urls)    # 利用 set 去重 urls
 
+
 lock = threading.Lock()     # 全局资源锁
 
 
 def urls_crawler(url):
-    """ 爬虫入口，主要爬取操作 """
+    """ 爬虫入口，主要爬取操作
+    """
     try:
-        r = requests.get(url, headers=headers, timeout=10).text
+        r = requests.get(url, headers=HEADERS, timeout=10).text
         folder_name = BeautifulSoup(r, 'lxml').find(
             'div', class_="main-image").find('img')['alt'].replace("?", " ")
         with lock:
@@ -51,9 +57,10 @@ def urls_crawler(url):
 
                 for _, page_url in enumerate(page_urls):
                     time.sleep(0.6)
-                    result = requests.get(page_url, headers=headers, timeout=10).text
+                    result = requests.get(page_url, headers=HEADERS, timeout=10).text
                     img_url = BeautifulSoup(result, 'lxml').find(
-                        'div', class_="main-image").find('p').find('a').find('img')['src']
+                        'div', class_="main-image").find(
+                        'p').find('a').find('img')['src']
                     img_urls.append(img_url)
                 for cnt, url in enumerate(img_urls):
                     save_pic(url, cnt)
@@ -62,9 +69,10 @@ def urls_crawler(url):
 
 
 def save_pic(pic_src, pic_cnt):
-    """ 保存图片到本地 """
+    """ 保存图片到本地
+    """
     try:
-        img = requests.get(pic_src, headers=headers, timeout=10)
+        img = requests.get(pic_src, headers=HEADERS, timeout=10)
         imgname = "pic_cnt_{}.jpg".format(pic_cnt + 1)
         with open(imgname, 'ab') as f:
             f.write(img.content)
@@ -74,8 +82,9 @@ def save_pic(pic_src, pic_cnt):
 
 
 def make_dir(folder_name):
-    """ 新建文件夹并切换到该目录下 """
-    path = os.path.join(dir_path, folder_name)
+    """ 新建文件夹并切换到该目录下
+    """
+    path = os.path.join(DIR_PATH, folder_name)
     # 如果目录已经存在就不用再次爬取了，去重，提高效率。存在返回 False，否则反之
     if not os.path.exists(path):
         os.makedirs(path)
@@ -88,7 +97,8 @@ def make_dir(folder_name):
 
 def delete_empty_dir(dir):
     """ 如果程序半路中断的话，可能存在已经新建好文件夹但是仍没有下载的图片的情况
-    但此时文件夹已经存在所以会忽略该套图的下载，此时要删除空文件夹 """
+    但此时文件夹已经存在所以会忽略该套图的下载，此时要删除空文件夹
+    """
     if os.path.exists(dir):
         if os.path.isdir(dir):
             for d in os.listdir(dir):
@@ -99,15 +109,16 @@ def delete_empty_dir(dir):
             os.rmdir(dir)
             print("remove the empty dir: {}".format(dir))
     else:
-        print("Please start your performance!") # 请开始你的表演
+        print("Please start your performance!")     # 请开始你的表演
+
 
 if __name__ == "__main__":
     urls = get_urls()
     pool = Pool(processes=cpu_count())
     try:
-        delete_empty_dir(dir_path)
+        delete_empty_dir(DIR_PATH)
         pool.map(urls_crawler, urls)
     except Exception as e:
         time.sleep(30)
-        delete_empty_dir(dir_path)
+        delete_empty_dir(DIR_PATH)
         pool.map(urls_crawler, urls)
