d8a3acd4e01af30cdc1308b97f8a1ae41e98b882
chenjiandongx <chenjiandongx@qq.com>
2018-06-06 20:51:25 +0800

    Add: 新增 requirements.txt

--- analysis.py
+++ analysis.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 
 
-# 将所有文件夹名转换为str类型
-folder_name = " ".join(os.listdir(r"E:\Pic\mzitu"))
+# 将所有文件夹名转换为 str 类型
+folder_name = " ".join(os.listdir(r"D:\mzitu"))
 
-# jieba分词
+# jieba 分词
 jieba.load_userdict(r".\data\jieba.txt")
 seg_list = jieba.lcut(folder_name, cut_all=False)
 
@@ -18,7 +18,9 @@ for seg in seg_list:
     counter[seg] = counter.get(seg, 1) + 1
 print(counter)
 # 根据词频排序字典
-counter_sort = sorted(counter.items(), key=lambda value: value[1], reverse=True)
+counter_sort = sorted(
+    counter.items(), key=lambda value: value[1], reverse=True
+)
 # print(counter_sort)
 
 # 解析成 json 类型并写入文件
@@ -27,9 +29,12 @@ with open(r".\data\words.json", "w+", encoding="utf-8") as f:
     f.write(words)
 
 # 生成词云
-wordcloud = WordCloud(font_path=r".\font\msyh.ttf",
-                      max_words=100, height=600, width=1200).generate_from_frequencies(counter)
+wordcloud = WordCloud(
+    font_path=r".\font\msyh.ttf", max_words=100, height=600, width=1200
+).generate_from_frequencies(
+    counter
+)
 plt.imshow(wordcloud)
-plt.axis('off')
+plt.axis("off")
 plt.show()
-wordcloud.to_file('worldcloud.jpg')
+wordcloud.to_file("worldcloud.jpg")
--- mzitu.py
+++ mzitu.py
@@ -14,14 +14,16 @@ HEADERS = {
     'Referer': 'http://www.mzitu.com'
 }
 
-DIR_PATH = r"D:\mzitu"      # 下载图片保存路径
+# 下载图片保存路径
+DIR_PATH = r"D:\mzitu"
 
 
 def get_urls():
-    """ 获取 mzitu 网站下所有套图的 url
+    """
+    获取 mzitu 网站下所有套图的 url
     """
     page_urls = ['http://www.mzitu.com/page/{cnt}'.format(cnt=cnt)
-                 for cnt in range(1, 143)]
+                 for cnt in range(1, 193)]
     print("Please wait for second ...")
     img_urls = []
     for page_url in page_urls:
@@ -41,7 +43,8 @@ lock = threading.Lock()     # 全局资源锁
 
 
 def urls_crawler(url):
-    """ 爬虫入口，主要爬取操作
+    """
+    爬虫入口，主要爬取操作
     """
     try:
         r = requests.get(url, headers=HEADERS, timeout=10).text
@@ -56,7 +59,7 @@ def urls_crawler(url):
                 img_urls = []
 
                 for _, page_url in enumerate(page_urls):
-                    time.sleep(0.6)
+                    time.sleep(0.25)
                     result = requests.get(page_url, headers=HEADERS, timeout=10).text
                     img_url = BeautifulSoup(result, 'lxml').find(
                         'div', class_="main-image").find(
@@ -69,20 +72,23 @@ def urls_crawler(url):
 
 
 def save_pic(pic_src, pic_cnt):
-    """ 保存图片到本地
+    """
+    保存图片到本地
     """
     try:
+        time.sleep(0.10)
         img = requests.get(pic_src, headers=HEADERS, timeout=10)
-        imgname = "pic_cnt_{}.jpg".format(pic_cnt + 1)
-        with open(imgname, 'ab') as f:
+        img_name = "pic_cnt_{}.jpg".format(pic_cnt + 1)
+        with open(img_name, 'ab') as f:
             f.write(img.content)
-            print(imgname)
+            print(img_name)
     except Exception as e:
         print(e)
 
 
 def make_dir(folder_name):
-    """ 新建文件夹并切换到该目录下
+    """
+    新建文件夹并切换到该目录下
     """
     path = os.path.join(DIR_PATH, folder_name)
     # 如果目录已经存在就不用再次爬取了，去重，提高效率。存在返回 False，否则反之
@@ -95,19 +101,20 @@ def make_dir(folder_name):
     return False
 
 
-def delete_empty_dir(dir):
-    """ 如果程序半路中断的话，可能存在已经新建好文件夹但是仍没有下载的图片的情况
-    但此时文件夹已经存在所以会忽略该套图的下载，此时要删除空文件夹
+def delete_empty_dir(save_dir):
     """
-    if os.path.exists(dir):
-        if os.path.isdir(dir):
-            for d in os.listdir(dir):
-                path = os.path.join(dir, d)     # 组装下一级地址
+    如果程序半路中断的话，可能存在已经新建好文件夹但是仍没有下载的图片的
+    情况但此时文件夹已经存在所以会忽略该套图的下载，此时要删除空文件夹
+    """
+    if os.path.exists(save_dir):
+        if os.path.isdir(save_dir):
+            for d in os.listdir(save_dir):
+                path = os.path.join(save_dir, d)     # 组装下一级地址
                 if os.path.isdir(path):
                     delete_empty_dir(path)      # 递归删除空文件夹
-        if not os.listdir(dir):
-            os.rmdir(dir)
-            print("remove the empty dir: {}".format(dir))
+        if not os.listdir(save_dir):
+            os.rmdir(save_dir)
+            print("remove the empty dir: {}".format(save_dir))
     else:
         print("Please start your performance!")     # 请开始你的表演
 
@@ -118,7 +125,7 @@ if __name__ == "__main__":
     try:
         delete_empty_dir(DIR_PATH)
         pool.map(urls_crawler, urls)
-    except Exception as e:
+    except Exception:
         time.sleep(30)
         delete_empty_dir(DIR_PATH)
         pool.map(urls_crawler, urls)
